98af28064a80106376841475673f919bf32b2a47
jongHyeon <diekfk1@naver.com>
2025-03-27 00:13:45 +0900

    백준 2741번 주석 추가

--- src/baekjoon/class1/BaekJoon2741.java
+++ src/baekjoon/class1/BaekJoon2741.java
@@ -2,6 +2,13 @@ package baekjoon.class1;
 
 import java.io.IOException;
 
+/**
+ * 문제
+ * 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+ */
 public class BaekJoon2741 {
     private static final int ASCII_ENTER = 10;
 
