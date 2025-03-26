26a5d41d29921a0b1a9b006e75de9d31303720e8
jongHyeon <diekfk1@naver.com>
2025-03-27 01:22:43 +0900

    백준 2920번 문제 풀이

---
+++ src/baekjoon/class1/BaekJoon2920.java
@@ -0,0 +1,50 @@
+package baekjoon.class1;
+
+import java.io.IOException;
+
+/**
+ * 문제
+ * 다장조는 c d e f g a b C, 총 8개 음으로 이루어져있다.
+ * 이 문제에서 8개 음은 다음과 같이 숫자로 바꾸어 표현한다. c는 1로, d는 2로, ..., C를 8로 바꾼다.
+ * 1부터 8까지 차례대로 연주한다면 ascending, 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면 mixed 이다.
+ * 연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 8개 숫자가 주어진다. 이 숫자는 문제 설명에서 설명한 음이며, 1부터 8까지 숫자가 한 번씩 등장한다.
+ */
+public class BaekJoon2920 {
+    private static final int ASCII_INPUT = 32;
+
+    public static void main(String[] args) {
+        String answer = "mixed";
+        boolean isSort = true;
+        int num = readInt();
+        if (num == 1) {
+            for (int i = 2; i <= 8; i++) {
+                if (readInt() != i) isSort = false;
+            }
+
+            if (isSort) answer = "ascending";
+        }
+        if (num == 8) {
+            for (int i = 7; i >= 1; i--) {
+                if (readInt() != i) isSort = false;
+            }
+
+            if (isSort) answer = "descending";
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int readInt() {
+        int asciiCode, sum = 0;
+        try {
+            while ((asciiCode = System.in.read()) > ASCII_INPUT) {
+                sum = (sum << 3) + (sum << 1) + (asciiCode & 15);   // sum = sum * 10 + (asciiCode - 48)
+            }
+        } catch (IOException ignored) {}
+
+        return sum;
+    }
+}
