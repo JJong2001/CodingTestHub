8d9c4eb7e74597613c8c181500b1837b165cd0b9
jongHyeon <diekfk1@naver.com>
2025-03-28 07:38:28 +0900

    백준 10172번 문제 주석 추가

--- src/baekjoon/class1/BaekJoon10172.java
+++ src/baekjoon/class1/BaekJoon10172.java
@@ -1,5 +1,12 @@
 package baekjoon.class1;
 
+/**
+ * 문제
+ * 아래 예제와 같이 개를 출력하시오.
+ *
+ * 입력
+ * 없음.
+ */
 public class BaekJoon10172 {
     public static void main(String[] args) {
         System.out.println(
