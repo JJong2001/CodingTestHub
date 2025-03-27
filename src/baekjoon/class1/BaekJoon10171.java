2c0a4c536bf926352b50307649fe9c6122d80725
jongHyeon <diekfk1@naver.com>
2025-03-28 06:47:34 +0900

    백준 10171번 문제 풀이

---
+++ src/baekjoon/class1/BaekJoon10171.java
@@ -0,0 +1,19 @@
+package baekjoon.class1;
+
+/**
+ * 문제
+ * 아래 예제와 같이 고양이를 출력하시오.
+ *
+ * 입력
+ * 없음.
+ */
+public class BaekJoon10171 {
+    public static void main(String[] args) {
+        System.out.println(
+                "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|"
+        );
+    }
+}
