343b9bbf5be17489d62a7f54a5d6aabf6d574097
jongHyeon <diekfk1@naver.com>
2025-03-30 11:38:42 +0900

    백준 11654번 문제 풀이

---
+++ src/baekjoon/class1/BaekJoon11654.java
@@ -0,0 +1,17 @@
+package baekjoon.class1;
+
+import java.io.IOException;
+
+/**
+ * 문제
+ * 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때,
+ * 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+ */
+public class BaekJoon11654 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.in.read());
+    }
+}
