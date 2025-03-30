23ce33aaf2e38f788bc68e7f712c4871c03596a3
jongHyeon <diekfk1@naver.com>
2025-03-30 11:50:51 +0900

    백준 25083번 문제 풀이

---
+++ src/baekjoon/class1/BaekJoon25083.java
@@ -0,0 +1,21 @@
+package baekjoon.class1;
+
+/**
+ * 문제
+ * 아래 예제와 같이 새싹을 출력하시오.
+ *
+ * 입력
+ * 입력은 없다.
+ */
+public class BaekJoon25083 {
+    public static void main(String[] args) {
+        System.out.println(
+                "         ,r'\"7\n" +
+                        "r`-_   ,'  ,/\n" +
+                        " \\. \". L_r'\n" +
+                        "   `~\\/\n" +
+                        "      |\n" +
+                        "      |"
+        );
+    }
+}
